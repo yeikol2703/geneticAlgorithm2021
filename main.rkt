33bc25a286abacae7ca1d9f6f7eb105ad2530a7c
Yeikol Villalobos <yeikolvillalobos27@gmail.com>
2021-11-06 12:19:20 -0600

    05-11-2021 con chester

--- main.rkt
+++ main.rkt
@@ -1,131 +1,130 @@
 #lang racket
 
-(define (knapsack lst)
+;*************** Datos de prueba ***************
 
-  (define half (/ (apply + lst) 2))   ; compute half of total
-  (printf "list  : ~a\nhalf  : ~a\n" lst half)
-
-  (define (combs lst1 (lst2 null))    ; compute all the combinations  
-    (if (null? lst1)
-        (if (null? lst2) 
-            null
-            (list (reverse lst2)))
-        (append
-         (combs (cdr lst1) lst2)      ; case 1 -> we don't carry the iten
-         (combs (cdr lst1) (cons (car lst1) lst2)))))   ; case 2 -> we do
-
-  (for/fold ((delta half) (res null)) ((c (in-list (combs lst)))) ; determine the best fit
-    (let* ((sm (apply + c)) (newdelta (abs (- half sm))))
-      (cond
-        ((< newdelta delta) (values newdelta (list c)))
-        ((= newdelta delta) (values    delta (cons c res)))
-        (else               (values    delta res))))))
+(define banco '(1 2 3 5 6 9 32 1 2 5 12 31 15)) ;lista de genes
+(define caracteristica '(30 60 90))
+(define individuo '(((2 6 2) 10) ((9 5 1 1 5) 21) ((3 31 15) 49) ((12 32) 44)))
 
 
- (let-values (((delta res) (knapsack (cdr (range 0 24 5)))))
-   (printf "result: ~a\ndelta : ~a\n" res delta))
+;*************** Funciones ***************
 
+; Sumna la lista y da un valor
+; P: L = Cualquier lista
+; S: #Total
+(define (sumarLista L)
+   (if (empty? L) 0 
+       (+ (car L) (sumarLista (cdr L))) 
+   )
+)
 
+; Saca el valor optimo de la generacion
+; P: g = grupos L: Lista de genes '(# # # # #...#)
+; S: #valorOptimo
+(define valorOptimo 
+  (lambda (g L)
+    (/ (sumarLista L) g)
+    )
+  )
 
-;============================
-(define resolver
-  (lambda ( m L);recibe parametro grupos y la lista L
-    (resolver-x L m '());llama a otra funcion y le pasa una lista L, el numero de grupos y otra lista A
-    ))
+;*************** Genes ***************
 
-(define resolver-x
-  (lambda (L m A) ;lista, el numero de grupos y otra lista
-    (if (null? L) A ; si la primera lista L esta vacia imprime la lista A vacia
-        (resolver-x (cdr L) m (add (car L) m A)) ;llama recursivamente agarra el segundo elemento de L, numero de grupos, llama a add
-        )
-    ))
+; Produce una caracteristica del individuo. 
+; P: L = Lista del grupo de genes de la caracteristcia
+; S: '(((# # ... #) #Total)
+(define (generarCaracteristica L) 
+  (if (empty? L) '()
+      (cons L (list (sumarLista L)))
+   )
+)
 
-(define add
-  (lambda (x m A) ;valor, numero de grupos y lista A
-    (cond ((null? A) (list (list x))) 
+;*************** Individuo ***************
+
+(define add 
+  (lambda (x m A)
+    (cond ((null? A) (list (list x)))
           ((= (remainder x m) (remainder (caar A) m))
            (cons (cons x (car A)) (cdr A)))
           (else
            (cons (car A) (add x m (cdr A)))))
     ))
 
-(define shuffle
-  (lambda (list)
-    (if (< (length list) 2) 
-        list
-        (let ((item (list-ref list (random (length list)))))
-          (cons item (shuffle (remove item list)))))))
+(define listarIndv-x
+  (lambda (L m A)
+    (if (null? L) A
+        (listarIndv-x (cdr L) m (add (car L) m A)))
+    ))
 
-; 4 es cantidad de grupos '() el list inicial
-(resolver 4 '(30 60 90 25 20 15 16 120 200 43 18 43 18 30 30) )
+; Genera la lista de n grupos segun los genes de la lista
+; P: L = Lista de banco de genes
+; S: '((# # #) (# # #...#) (# # #) (# # #)...)
+(define listarIndv
+  (lambda ( m L)
+    (listarIndv-x (shuffle L) m '())
+    ))
 
-;genera lista
-(define lista(list 30 60 90 25 20 15 16 120 200 43 18 43 18 30 30))
+;Genera el individuo apartir de los grupos de genes
+;Utiliza la funcion *listarIndv*
+;P: L = lista de genes en grupos
+;S: '(((# # #) #T) ((# # #) #T) ((# # #) #T) ((# # #) #T))
+(define (generarIndiv L)
+  (if (empty? L) '()
+      (cons (generarCaracteristica (car L)) (generarIndiv (cdr L)))
+      )
+ )
 
-;genera 5 individuos con 4 grupos cada uno de forma aleatoria
-(resolver 4 (shuffle lista)  )
-(resolver 4 (shuffle lista)  )
-(resolver 4 (shuffle lista)  )
-(resolver 4 (shuffle lista)  )
-;======================================================
+;Genera la lista de los valores de las caracteristicas del individuo
+(define (valoresCaractIndv L)
+  (if (empty? L)'()
+      (cons (cadar L) (valoresCaractIndv (cdr L)))
+      )
+ )
 
-;funcion que suma todos los elementos de una lista
-;suma los elementos de la lista
-(define (suma lista)
- (if (empty? lista)
-     0
-    (+ (car lista) (suma (cdr lista)))
+
+(define restarOptimo
+  (lambda (L c)
+    (map (lambda (x) (abs(- (valorOptimo 4 L) x ))) (valoresCaractIndv c)   )
+    )
+ )
+
+;sumarLista (restarOptimo banco (cadar lst) )
+
+;(restarOptimo banco individuo)
+
+
+(define (quicksort lst gen )
+  (cond
+    ((or (null? lst) ; empty list is sorted
+         (null? (cdr lst))) ; single-element list is sorted
+     lst)
+    (else
+      (let ((pivot (sumarLista (restarOptimo gen (car lst) ))) ; Select the first element as the pivot
+            (rest (cdr lst)))
+        (append
+          (quicksort ; Recursively sort the list of smaller values
+            (filter (lambda (x) (< (sumarLista (restarOptimo gen x )) pivot)) rest) gen) ; Select the smaller values
+          (list pivot) ; Add the pivot in the middle
+          (quicksort ; Recursively sort the list of larger values
+            (filter (lambda (x) (>= (sumarLista (restarOptimo gen  x)) pivot)) rest) gen)))))) ; Select the larger and equal values
+
+
+
+; *************** Generacion ***************
+
+; Genera la primera generacion apartir de la lista de genes
+; P: a = poblacion / l = lista de genes / g = grupos
+; S: generacion de individos
+(define primeraGeneracion 
+  (lambda (a l g) 
+    (if (zero? a) '()
+        (cons
+         (generarIndiv (listarIndv g l)) (primeraGeneracion (- a 1) l g)
+        )
     )
   )
-;llama la funcion
-(suma '(30 60 90 25 20 15 16 120 200 43 18 43 18 30 30))
+)
 
+;(cdaar (primeraGeneracion 4 banco 4))
 
-;funcion para recorrer y evaluar cada elemento en la lista
-
-(define (recorrer lista)
-(for-each (lambda (x)
-            (display x))
-          (list lista)
-          )
-  )
-
-(recorrer lista)
-
-;funcion para saber el length de una lista
-( define ( longitud L )
- ( cond
- [( empty? L ) 0]
- [ else (+ 1 ( longitud ( rest L ) ) ) ]) )
-
-(longitud lista)
-
-;
-
-(define a(list 1 2 3 4 2))
-(define b(list 1 2 3 4 3))
-(define c(list 1 2 3 4 4))
-
-
-(define listota(list a))
-
-(define attach-at-end
-  (lambda (val li)
-    (if (null? li)
-        (list val)
-        (cons (car li) (attach-at-end val (cdr li)))
-
-        )))
-
-(longitud listota)
-
-
-(attach-at-end lista listota)
-
-(recorrer (attach-at-end lista listota))
-
-
-
-
-
-
+;(primeraGeneracion 4 banco 4)
+;(quicksort (primeraGeneracion 4 banco 4) banco )
