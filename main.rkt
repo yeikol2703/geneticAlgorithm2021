cbadc7f8ea92585509c5e26dbac5c7ae168f3534
William Arias <willi.am@hotmail.es>
2021-11-09 15:57:09 -0600

    Format

--- main.rkt
+++ main.rkt
@@ -1,28 +1,49 @@
 #lang racket
 
+;
+;
+;===============================================
+;
+; (c) 2021
+; version 1.0.0 2021-11-09
+;
+;
+;-----------------------------------------------
+; EIF400 Paradigmas de Programación
+; 2do ciclo 2021, Grupo 50043 y 50042
+; Proyecto 1
+; 
+;
+; 207920434 Francisco Fernandez Rojas - Grupo 3pm
+; 702670531 Yeikol Villalobos Herrera - Grupo 3pm 
+; 117350395 William Arias Bermudez - Grupo 8pm
+;
+; ===============================================
+;
+
 ;*************** Datos de prueba ***************
 
 (define banco '(1 2 3 5 6 9 32 1 2 5 12 31 15)) ;lista de genes
-(define caracteristica '(30 60 90))
-(define individuo '(((2 6 2) 10) ((9 5 1 1 5) 21) ((3 31 15) 49) ((12 32) 44)))
+(define characteristic '(30 60 90))
+(define indv '(((2 6 2) 10) ((9 5 1 1 5) 21) ((3 31 15) 49) ((12 32) 44)))
 
 ;*************** Funciones ***************
 
-; Sumna la lista y da un valor
+; Suma la lista y da un valor
 ; P: L = Cualquier lista
 ; S: #Total
-(define (sumarLista L)
+(define (sumList L)
    (if (empty? L) 0 
-       (+ (car L) (sumarLista (cdr L))) 
+       (+ (car L) (sumList (cdr L))) 
    )
 )
 
 ; Saca el valor optimo de la generacion
 ; P: g = grupos L: Lista de genes '(# # # # #...#)
 ; S: #valorOptimo
-(define valorOptimo 
+(define getOptimum 
   (lambda (g L)
-    (/ (sumarLista L) g)
+    (/ (sumList L) g)
     )
   )
 
@@ -31,9 +52,9 @@
 ; Produce una caracteristica del individuo. 
 ; P: L = Lista del grupo de genes de la caracteristcia
 ; S: '(((# # ... #) #Total)
-(define (generarCaracteristica L) 
+(define (breedCharacteristic L) 
   (if (empty? L) '()
-      (cons L (list (sumarLista L)))
+      (cons L (list (sumList L)))
    )
 )
 
@@ -48,47 +69,47 @@
            (cons (car A) (add x m (cdr A)))))
     ))
 
-(define listarIndv-x
+(define listIndv-x
   (lambda (L m A)
     (if (null? L) A
-        (listarIndv-x (cdr L) m (add (car L) m A)))
+        (listIndv-x (cdr L) m (add (car L) m A)))
     ))
 
 ; Genera la lista de n grupos segun los genes de la lista
 ; P: L = Lista de banco de genes
 ; S: '((# # #) (# # #...#) (# # #) (# # #)...)
-(define listarIndv
+(define listIndv
   (lambda ( m L)
-    (listarIndv-x (shuffle L) m '())
+    (listIndv-x (shuffle L) m '())
     ))
 
 ;Genera el individuo apartir de los grupos de genes
 ;Utiliza la funcion *listarIndv*
 ;P: L = lista de genes en grupos
 ;S: '(((# # #) #T) ((# # #) #T) ((# # #) #T) ((# # #) #T))
-(define (generarIndiv L)
+(define (breedIndv L)
   (if (empty? L) '()
-      (cons (generarCaracteristica (car L)) (generarIndiv (cdr L)))
+      (cons (breedCharacteristic (car L)) (breedIndv (cdr L)))
       )
  )
 
 ;Genera la lista de los valores de las caracteristicas del individuo
-(define (valoresCaractIndv L)
+(define (generateValues L)
   (if (empty? L)'()
-      (cons (cadar L) (valoresCaractIndv (cdr L)))
+      (cons (cadar L) (generateValues (cdr L)))
       )
  )
 
 
-(define restarOptimo
+(define deductOptimum
   (lambda (L c)
-    (map (lambda (x) (abs(- (valorOptimo 4 L) x ))) (valoresCaractIndv c)   )
+    (map (lambda (x) (abs(- (getOptimum 4 L) x ))) (generateValues c)   )
     )
  )
 
-;sumarLista (restarOptimo banco (cadar lst) )
+;sumarLista (deductOptimum banco (cadar lst) )
 
-;(restarOptimo banco individuo)
+;(deductOptimum banco individuo)
 
 
 (define (quicksort lst gen )
@@ -97,14 +118,14 @@
          (null? (cdr lst))) ; single-element list is sorted
      lst)
     (else
-      (let ((pivot (sumarLista (restarOptimo gen (car lst) ))) ; Select the first element as the pivot
+      (let ((pivot (sumList (deductOptimum gen (car lst) ))) ; Select the first element as the pivot
             (rest (cdr lst)))
         (append
           (quicksort ; Recursively sort the list of smaller values
-            (filter (lambda (x) (< (sumarLista (restarOptimo gen x )) pivot)) rest) gen) ; Select the smaller values
+            (filter (lambda (x) (< (sumList (deductOptimum gen x )) pivot)) rest) gen) ; Select the smaller values
           (list pivot) ; Add the pivot in the middle
           (quicksort ; Recursively sort the list of larger values
-            (filter (lambda (x) (>= (sumarLista (restarOptimo gen  x)) pivot)) rest) gen)))))) ; Select the larger and equal values
+            (filter (lambda (x) (>= (sumList (deductOptimum gen  x)) pivot)) rest) gen)))))) ; Select the larger and equal values
 
 
 
@@ -113,17 +134,17 @@
 ; Genera la primera generacion apartir de la lista de genes
 ; P: a = poblacion / l = lista de genes / g = grupos
 ; S: generacion de individos
-(define primeraGeneracion 
+(define firstGen 
   (lambda (a l g) 
     (if (zero? a) '()
         (cons
-         (generarIndiv (listarIndv g l)) (primeraGeneracion (- a 1) l g)
+         (breedIndv (listIndv g l)) (firstGen (- a 1) l g)
         )
     )
   )
 )
 
-;(cdaar (primeraGeneracion 4 banco 4))
+;(cdaar (firstGen 4 banco 4))
 (print "INICIO")
 (newline)
 (print "================================================== ==================================================")
@@ -132,12 +153,12 @@
 (newline)
 (print "====================================================================================================")
 (newline)
-(primeraGeneracion 4 banco 4)
+(firstGen 4 banco 4)
 (print "====================================================================================================")
 (newline)
 (print "Individuo  optimo es: ")
 (newline)
-(quicksort (primeraGeneracion 4 banco 4) banco )
+(quicksort (firstGen 4 banco 4) banco )
 (print "================================================== ==================================================")
 (newline)
 (print "FINAL")
